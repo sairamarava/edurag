c8dba9acc2360799f1d31229b24e9fcf707b1913
sairamarava <sairamrdya@gmail.com>
2026-02-22 18:31:30 +0530

    Added cors origin for frontend vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://your-frontend-name.vercel.app"
+        "https://edurag-iota.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
